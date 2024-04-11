a7d77774485ef5e930bdcc7d86fb43283a65fbf7
JongHoPark <qkrwhdgh126@gmail.com>
2024-04-11 23:46:33 +0900

    feat: mainpic 다른버전

--- yfc-web/src/components/mainpic/mainpic.js
+++ yfc-web/src/components/mainpic/mainpic.js
@@ -1,12 +1,16 @@
 import React from "react";
 import "./mainpic.scss";
 import MainPhoto from "../../assets/images/mainphoto2.png";
+import MainPhoto1 from "../../assets/images/mainpic.png";
 
 export default function MainPic() {
+  window.addEventListener("resize", () => {
+    console.log(window.innerHeight);
+  });
   return (
     <div className="mainpic">
       <div className="main">
-        <img src={MainPhoto} alt="" />
+        <img src={MainPhoto1} alt="" />
         <div className="black">
           <div className="txtbox">
             <p>En Garde, Prêts? Allez!</p>
