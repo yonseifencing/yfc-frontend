932c12e7640fd423ef1a985bf26000ae7a4a1091
JongHoPark <qkrwhdgh126@gmail.com>
2024-05-31 12:34:04 +0900

    feat: 인증번호 출석

--- yfc-web/src/pages/attend/attend.js
+++ yfc-web/src/pages/attend/attend.js
@@ -1,13 +1,35 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import "./attend.scss";
 import { FaRegCheckCircle } from "react-icons/fa";
 
 export default function AttendPage() {
   const [attendState, setAttentState] = useState("미인증");
 
+  const input1Ref = useRef();
+  const input2Ref = useRef();
+  const input3Ref = useRef();
+  const input4Ref = useRef();
+
   const handleClickAttend = () => {
     setAttentState("출석");
   };
+
+  const handleWriteInput = (e) => {
+    if (e.target.value.length == 2) {
+      e.target.value = e.target.value.slice(0, -1);
+    }
+  };
+
+  const moveFocusNext = (num) => {
+    if (num == 1) {
+      input2Ref.current.focus();
+    } else if (num == 2) {
+      input3Ref.current.focus();
+    } else if (num == 3) {
+      input4Ref.current.focus();
+    }
+  };
+
   return (
     <div className="attendpage">
       <div className="userversion">
@@ -41,9 +63,53 @@ export default function AttendPage() {
             </div>
           </div>
         </div> */}
-        <button className="attendbtn" onClick={handleClickAttend}>
-          인증번호 입력
-        </button>
+        <form>
+          <div className="inputs">
+            <input
+              ref={input1Ref}
+              type="number"
+              onChange={(e) => {
+                handleWriteInput(e);
+                if (e.target.value.length != 0) {
+                  moveFocusNext(1);
+                }
+              }}
+            />
+            <input
+              ref={input2Ref}
+              type="number"
+              onChange={(e) => {
+                handleWriteInput(e);
+                if (e.target.value.length != 0) {
+                  moveFocusNext(2);
+                }
+              }}
+            />
+            <input
+              ref={input3Ref}
+              type="number"
+              onChange={(e) => {
+                handleWriteInput(e);
+                if (e.target.value.length != 0) {
+                  moveFocusNext(3);
+                }
+              }}
+            />
+            <input
+              ref={input4Ref}
+              type="number"
+              onChange={(e) => {
+                handleWriteInput(e);
+                if (e.target.value.length != 0) {
+                  moveFocusNext(4);
+                }
+              }}
+            />
+          </div>
+          <button className="attendbtn" onClick={handleClickAttend}>
+            인증번호 입력
+          </button>
+        </form>
       </div>
       {/* <div className="adminversion">인증번호 생성</div> */}
     </div>
